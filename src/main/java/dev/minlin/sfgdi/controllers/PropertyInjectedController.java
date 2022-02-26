c1b2294f30826093486d39e95db5829b50c74498
linminca <min-lin@hotmail.com>
2022-02-25 22:43:09 -0500

    Differentiate property/setter/constructorInjected Controllers

---
+++ src/main/java/dev/minlin/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,11 @@
+package dev.minlin.sfgdi.controllers;
+
+import dev.minlin.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+    public GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
